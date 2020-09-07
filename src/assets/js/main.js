1e2c3017fc19076f8d1c8d1f1c1e6bc90d4b84f7
Eun Kang <choa030@gmail.com>
2020-09-07 20:46:26 +0900

    dd

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,17 +1,6 @@
 ///for IE 11
-
-// Production steps of ECMA-262, Edition 6, 22.1.2.1
 if (!Array.from) {
     Array.from = (function() {
-        var symbolIterator;
-        try {
-            symbolIterator = Symbol.iterator
-                ? Symbol.iterator
-                : "Symbol(Symbol.iterator)";
-        } catch {
-            symbolIterator = "Symbol(Symbol.iterator)";
-        }
-
         var toStr = Object.prototype.toString;
         var isCallable = function(fn) {
             return (
@@ -21,8 +10,12 @@ if (!Array.from) {
         };
         var toInteger = function(value) {
             var number = Number(value);
-            if (isNaN(number)) return 0;
-            if (number === 0 || !isFinite(number)) return number;
+            if (isNaN(number)) {
+                return 0;
+            }
+            if (number === 0 || !isFinite(number)) {
+                return number;
+            }
             return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
         };
         var maxSafeInteger = Math.pow(2, 53) - 1;
@@ -31,67 +24,17 @@ if (!Array.from) {
             return Math.min(Math.max(len, 0), maxSafeInteger);
         };
 
-        var setGetItemHandler = function setGetItemHandler(isIterator, items) {
-            var iterator = isIterator && items[symbolIterator]();
-            return function getItem(k) {
-                return isIterator ? iterator.next() : items[k];
-            };
-        };
-
-        var getArray = function getArray(
-            T,
-            A,
-            len,
-            getItem,
-            isIterator,
-            mapFn
-        ) {
-            // 16. Let k be 0.
-            var k = 0;
-            // 17. Repeat, while k < len… or while iterator is done (also steps a - h)
-            while (k < len || isIterator) {
-                var item = getItem(k);
-                var kValue = isIterator ? item.value : item;
-
-                if (isIterator && item.done) {
-                    return A;
-                } else {
-                    if (mapFn) {
-                        A[k] =
-                            typeof T === "undefined"
-                                ? mapFn(kValue, k)
-                                : mapFn.call(T, kValue, k);
-                    } else {
-                        A[k] = kValue;
-                    }
-                }
-                k += 1;
-            }
-
-            if (isIterator) {
-                throw new TypeError(
-                    "Array.from: provided arrayLike or iterator has length more then 2 ** 52 - 1"
-                );
-            } else {
-                A.length = len;
-            }
-
-            return A;
-        };
-
         // The length property of the from method is 1.
-        return function from(arrayLikeOrIterator /*, mapFn, thisArg */) {
+        return function from(arrayLike /*, mapFn, thisArg */) {
             // 1. Let C be the this value.
             var C = this;
 
-            // 2. Let items be ToObject(arrayLikeOrIterator).
-            var items = Object(arrayLikeOrIterator);
-            var isIterator = isCallable(items[symbolIterator]);
-
+            // 2. Let items be ToObject(arrayLike).
+            var items = Object(arrayLike);
             // 3. ReturnIfAbrupt(items).
-            if (arrayLikeOrIterator == null && !isIterator) {
+            if (arrayLike == null) {
                 throw new TypeError(
-                    "Array.from requires an array-like object or iterator - not null or undefined"
+                    "Array.from requires an array-like object - not null or undefined"
                 );
             }
 
@@ -118,19 +61,30 @@ if (!Array.from) {
             var len = toLength(items.length);
 
             // 13. If IsConstructor(C) is true, then
-            // 13. a. Let A be the result of calling the [[Construct]] internal method
-            // of C with an argument list containing the single item len.
+            // 13. a. Let A be the result of calling the [[Construct]] internal method of C with an argument list containing the single item len.
             // 14. a. Else, Let A be ArrayCreate(len).
             var A = isCallable(C) ? Object(new C(len)) : new Array(len);
 
-            return getArray(
-                T,
-                A,
-                len,
-                setGetItemHandler(isIterator, items),
-                isIterator,
-                mapFn
-            );
+            // 16. Let k be 0.
+            var k = 0;
+            // 17. Repeat, while k < len… (also steps a - h)
+            var kValue;
+            while (k < len) {
+                kValue = items[k];
+                if (mapFn) {
+                    A[k] =
+                        typeof T === "undefined"
+                            ? mapFn(kValue, k)
+                            : mapFn.call(T, kValue, k);
+                } else {
+                    A[k] = kValue;
+                }
+                k += 1;
+            }
+            // 18. Let putStatus be Put(A, "length", len, true).
+            A.length = len;
+            // 20. Return A.
+            return A;
         };
     })();
 }
