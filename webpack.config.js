d8ce24b87264bbede7561d5eab7240f494f5ecbb
Eun Kang <choa030@gmail.com>
2020-09-07 21:34:14 +0900

    dd

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    entry: ["@babel/polyfill", __dirname + "/src/index.js"],
+    entry: {
+        home: ["@babel/polyfill", __dirname + "/src/index.js"],
+        second: ["@babel/polyfill", __dirname + "/src/assets/js/second.js"],
+        third: ["@babel/polyfill", __dirname + "/src/assets/js/third.js"]
+    },
     output: {
         path: __dirname + "/dist",
         publicPath: "/dist/",
-        filename: "bundle.js"
+        filename: "[name].js"
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            chunks: ["home"]
+        }),
+        new HtmlWebpackPlugin({
+            template: "src/2/index.html",
+            filename: "second.html",
+            chunks: ["second"]
+        }),
+        new HtmlWebpackPlugin({
+            template: "src/3/moderator/index.html",
+            filename: "third.html",
+            chunks: ["third"]
+        })
+    ],
     module: {
         rules: [
             {
